facfedec17f4bed794e67bb05e2e8f527405419d
Timo Bartels <timo@bartels-online.net>
2017-02-18 18:47:36 +0000

    fixed the test.sh

--- test.sh
+++ test.sh
@@ -15,8 +15,9 @@ for version in "${versions[@]}"; do
   [ -f "$version/Dockerfile" ] || continue
   docker build --rm -t tbartels/bartelsonline:"$version" "$version/."
   docker run -d --rm -p 80:80 --name bartelsonline tbartels/bartelsonline:"$version"
-  if ! curl 127.0.0.1/imprint.html | grep -q "2014"; then
+  if ! curl 127.0.0.1/imprint.html | grep 2014; then
     echo "Website test failed!"
     exit 1
   fi
+  docker stop bartelsonline
 done
