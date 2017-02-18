9747289c642f4eede99fda0bdeeaf8577aacadff
Timo Bartels <timo@bartels-online.net>
2017-02-18 18:49:59 +0000

    refactored test.sh

--- test.sh
+++ test.sh
@@ -14,10 +14,10 @@ versions=( "${versions[@]%/}" )
 for version in "${versions[@]}"; do
   [ -f "$version/Dockerfile" ] || continue
   docker build --rm -t tbartels/bartelsonline:"$version" "$version/."
-  docker run -d --rm -p 80:80 --name bartelsonline tbartels/bartelsonline:"$version"
+  docker run -d -p 80:80 --name bartelsonline tbartels/bartelsonline:"$version"
   if ! curl 127.0.0.1/imprint.html | grep 2014; then
     echo "Website test failed!"
     exit 1
   fi
-  docker stop bartelsonline
+  docker stop bartelsonline && docker rm bartelsonline
 done
