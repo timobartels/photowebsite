da130fe1804077bcdcc16a94b345285500a8e64c
Timo Bartels <timo@bartels-online.net>
2017-02-19 10:12:52 +0000

    removed test.sh and renamed it to build.sh

--- build.sh
+++ build.sh
@@ -14,10 +14,4 @@ versions=( "${versions[@]%/}" )
 for version in "${versions[@]}"; do
   [ -f "$version/Dockerfile" ] || continue
   docker build --rm -t tbartels/bartelsonline:"$version" "$version/."
-  docker run -d -p 80:80 --name bartelsonline tbartels/bartelsonline:"$version"
-  if ! curl 127.0.0.1:80/imprint.html | grep 2014; then
-    echo "Website test failed!"
-    exit 1
-  fi
-  docker stop bartelsonline && docker rm bartelsonline
 done
